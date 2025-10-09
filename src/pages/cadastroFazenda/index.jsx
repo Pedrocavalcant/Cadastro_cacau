e5753d0cdda5545232dcee930604105565fcca8d
Pedrocavalcant <pedrocavalcante50256@gmail.com>
2025-10-09 15:29:16 -0300

    Feat, incluído componentes na tela de cadastro de fazenda e de planta

--- src/pages/cadastroFazenda/index.jsx
+++ src/pages/cadastroFazenda/index.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import Logo from '../../assets/Logo.png'
+import cacau from '../../public/cacau.png'
+import Header from '../../components/Header'
+import Body from '../../components/Body'
 
-const CadastroFazenda = () => {
-  return (
-    <div> CadastroFazenda</div>
+const WelcomeLeft = () => {
+  return(
+    <>
+      <img src={Logo} alt="Logo" />
+      <div>
+        <h2 style={{lineHeight: 1.1}}>Cadastro de fazenda</h2>
+      </div>
+    </>
   )
 }
 
-export default CadastroFazenda
+
+
+export default function CadastroFazenda() {
+  
+  return (
+    <>
+      <Header/>
+      <Body left={<WelcomeLeft />} bgImage={cacau}>
+        <div></div>
+      </Body>
+    </>
+  )
+}
+
--- src/pages/cadastroPlanta/index.jsx
+++ src/pages/cadastroPlanta/index.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Logo from '../../assets/Logo.png'
+import cacau from '../../public/cacau.png'
+import Header from '../../components/Header'
+import Body from '../../components/Body'
+
+const WelcomeLeft = () => {
+  return (
+    <>
+      <img src={Logo} alt="Logo" />
+      <div>
+        <h2 style={{lineHeight: 1.1}}>Cadastro de planta</h2>
+      </div>
+    </>
+  )
+}
 
 const CadastroPlanta = () => {
   return (
-    <div>CadastroPlanta</div>
+    <>
+      <Header/>
+      <Body left={<WelcomeLeft/>} bgImage={cacau}>
+        <div></div>
+      </Body>
+    </>
   )
 }
 
