3b6ff85f37a0f2fce12c9502e7a46beac849e3df
Pedrocavalcant <pedrocavalcante50256@gmail.com>
2025-10-14 23:04:16 -0300

    Feat, validacoes da segunda tela de cadastro de funcionarios.

--- src/pages/cadastroUser2/index.jsx
+++ src/pages/cadastroUser2/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../assets/Logo.png";
 import cacau from "../../public/cacau.png";
 import Header from "../../components/Header";
@@ -22,6 +22,24 @@ const WelcomeLeft = () => {
 };
 
 const CadastroFuncionario2 = () => {
+  const [cpf, setCpf] = useState("")
+  const [celular, setCelular] = useState("")
+  const [rua, setRua] = useState("")
+  const [numeroCasa, setNumeroCasa] = useState("")
+  const [bairro, setBairro] = useState("")
+  const [cidade, setCidade] = useState("")
+  const [uf, setUf] = useState("")
+
+  const isCpf = cpf.trim().length === 11
+  const isCelular = celular.trim().length >= 10
+  const isRua = rua.trim().length > 0
+  const isNumeroCasa = numeroCasa.trim().length > 0
+  const isBairro = bairro.trim().length > 0
+  const isCidade = cidade.trim().length > 0
+  const isUf = uf.trim().length === 2
+
+  const isFormValid = isCpf && isCelular && isRua && isNumeroCasa && isBairro && isCidade && isUf
+
   const navigate = useNavigate();
 
   return (
@@ -34,7 +52,12 @@ const CadastroFuncionario2 = () => {
             <label className={style.labelTitle} htmlFor="cpf">
               CPF
             </label>
-            <input className={style.input} type="text" id="cpf" />
+            <input className={style.input} 
+            type="text" 
+            id="cpf" 
+            value={cpf}
+            onChange={(e) => setCpf(e.target.value)}
+            />
           </div>
 
           {/* Celular */}
@@ -42,7 +65,12 @@ const CadastroFuncionario2 = () => {
             <label className={style.labelTitle} htmlFor="celular">
               Número de celular
             </label>
-            <input className={style.input} type="text" id="celular" />
+            <input className={style.input} 
+            type="text" 
+            id="celular" 
+            value={celular}
+            onChange={(e) => setCelular(e.target.value)}
+            />
           </div>
 
           {/* Rua */}
@@ -50,7 +78,12 @@ const CadastroFuncionario2 = () => {
             <label className={style.labelTitle} htmlFor="rua">
               Rua
             </label>
-            <input className={style.input} type="text" id="rua" />
+            <input className={style.input} 
+            type="text" 
+            id="rua" 
+            value={rua}
+            onChange={(e) => setRua(e.target.value)}
+            />
           </div>
 
           {/* Nº da casa + Bairro */}
@@ -59,13 +92,24 @@ const CadastroFuncionario2 = () => {
               <label className={style.labelTitle} htmlFor="numCasa">
                 Nº da Casa
               </label>
-              <input className={style.input} type="text" id="numCasa" />
+              <input className={style.input} 
+              type="text" 
+              id="numCasa"
+              value={numeroCasa}
+              onChange={(e) => setNumeroCasa(e.target.value)}
+              />
             </div>
+
             <div className={style.field} style={{ flex: 0.58 }}>
               <label className={style.labelTitle} htmlFor="bairro">
                 Bairro
               </label>
-              <input className={style.input} type="text" id="bairro" />
+              <input className={style.input} 
+              type="text" 
+              id="bairro" 
+              value={bairro}
+              onChange={(e) => setBairro(e.target.value)}
+              />
             </div>
           </div>
 
@@ -75,13 +119,25 @@ const CadastroFuncionario2 = () => {
               <label className={style.labelTitle} htmlFor="cidade">
                 Cidade
               </label>
-              <input className={style.input} type="text" id="cidade" />
+              <input className={style.input} 
+              type="text" 
+              id="cidade" 
+              value={cidade}
+              onChange={(e) => setCidade(e.target.value)}
+              />
             </div>
+
             <div className={style.field} style={{ flex: 0.38 }}>
               <label className={style.labelTitle} htmlFor="uf">
                 UF
               </label>
-              <input className={style.input} type="text" id="uf" />
+              <input className={style.input} 
+              type="text" 
+              id="uf" 
+              value={uf}
+              onChange={(e) => setUf(e.target.value)}
+              maxLength={2}
+              />
             </div>
           </div>
 
@@ -94,7 +150,9 @@ const CadastroFuncionario2 = () => {
             >
               Voltar
             </button>
-            <button className={style.primaryBtn} type="button">
+            <button className={style.primaryBtn} 
+            disabled={!isFormValid}
+            type="button">
               Cadastrar
             </button>
           </div>
